f7e7c281052dcb44819cc5c5791a012560054dbe
Charzard4261 <CharzardX4261@gmail.com>
2022-03-01 23:44:17 +0000

    Update Version Number
    Update version number to 2.6.4

--- src/main/java/mikuhl/wikitools/WikiTools.java
+++ src/main/java/mikuhl/wikitools/WikiTools.java
@@ -36,7 +36,7 @@ import static jdk.nashorn.internal.objects.Global.Infinity;
 @Mod(modid = WikiTools.MODID, version = WikiTools.VERSION)
 public class WikiTools {
     public static final String MODID   = "wikitools";
-    public static final String VERSION = "2.6.3";
+    public static final String VERSION = "2.6.4";
 
     private static WikiTools        instance;
     public         WikiToolsConfigs configs;
