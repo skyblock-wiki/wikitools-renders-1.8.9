437102ed7e62034843e4c1421a4fb8c6991e3fb0
monkeyshk <monkeyshk-main@protonmail.com>
2022-09-29 03:28:01 +0800

    version 2.6.6
    - Fixed various UI item detection issues; changed default to no-fill
    - Now allows wikitext slot copying

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# wikitools
+# WikiTools
 Minecraft mod to copy NBT, render entities and other things.
 When Advanced Tooltips are enabled, the Skyblock ID will be shown in the item's tooltip.
 
@@ -8,7 +8,8 @@ To add items to the Copied Entity, press the Copy Entity key when hovering over 
 
 * Copy Entity
   * **Key**: M
-  * **Description**: Copies the entity you are looking at to memory.
+  * **Description**: Copies the entity you are looking at to memory as held item.
+  * **Holding Shift**: Copy the entity as head instead.
   
 * Open UI
   * **Key**: K
@@ -21,12 +22,14 @@ To add items to the Copied Entity, press the Copy Entity key when hovering over 
 * Copy Skull ID
   * **Key**: Z
   * **Description**: Copy a Skull's Texture ID (That links to http://textures.minecraft.net/texture/<ID>) to your clipboard. Works on placed Skulls,  Entities wearing skulls (excluding Players), and Skull items you are hovering over.
-  
+
 * Copy Wiki Tooltip
   * **Key**: X
-  * **Description**: Copy the item you are hovering over's generated Wiki Tooltip to your clipboard.
-  
-  
+  * **Description**: Copy the item you are hovering over as Wiki Inventory Slot Tooltip to your clipboard.
+  * **Holding Shift**: Copies tooltip as module code instead.
+
 * Copy Wiki UI Code
   * **Key**: C
   * **Description**: Copy the UI menu you have open into a Wiki UI format to your clipboard.
+  * **Holding Shift**: Enables "Filled Mode". This mode fills UI with the blank Black Stained Glass Pane by default.
+  * **Holding Ctrl**: Copies items in vanilla items instead of using the item name.
--- src/main/java/mikuhl/wikitools/handler/CopyNBTHandler.java
+++ src/main/java/mikuhl/wikitools/handler/CopyNBTHandler.java
@@ -1,5 +1,7 @@
 package mikuhl.wikitools.handler;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
 import mikuhl.wikitools.WikiToolsKeybinds;
 import mikuhl.wikitools.helper.ClipboardHelper;
 import mikuhl.wikitools.helper.FramebufferHelper;
@@ -36,8 +38,7 @@ import java.util.Map;
 public class CopyNBTHandler {
 
     @SubscribeEvent
-    public void onKeyInputEvent(InputEvent.KeyInputEvent event)
-    {
+    public void onKeyInputEvent(InputEvent.KeyInputEvent event) {
 
         if (!WikiToolsKeybinds.COPY_NBT.isKeyDown()) return;
 
@@ -45,28 +46,31 @@ public class CopyNBTHandler {
         MovingObjectPosition objectMouseOver = minecraft.objectMouseOver;
         Entity entity = objectMouseOver.entityHit;
 
-        if (entity != null)
-        {
-            if (entity instanceof EntityOtherPlayerMP)
-            {
+        if (entity != null) {
+            if (entity instanceof EntityOtherPlayerMP) {
                 NBTTagCompound nbt = new NBTTagCompound();
                 entity.writeToNBT(nbt);
+                // Code snippet from: https://github.com/cow-mc/Cowlection/blob/106168f533c627fef4f30ba1fece76c965778924/src/main/java/de/cowtipper/cowlection/command/MooCommand.java#L890
+                EntityOtherPlayerMP otherPlayer = (EntityOtherPlayerMP) entity;
+                if (otherPlayer.hasCustomName()) {
+                    nbt.setString("__customName", otherPlayer.getCustomNameTag());
+                }
+                GameProfile gameProfile = otherPlayer.getGameProfile();
+                for (Property property : gameProfile.getProperties().get("textures")) {
+                    nbt.setString("_skin", property.getValue());
+                }
                 ClipboardHelper.setClipboard(nbt);
                 Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessage(new ChatComponentText(I18n.format("wikitools.message.copiedNBT")));
-            } else
-            {
-                if (entity instanceof EntityItemFrame)
-                {
+            }
+            else {
+                if (entity instanceof EntityItemFrame) {
                     EntityItemFrame itemFrame = (EntityItemFrame) entity;
-                    if (itemFrame.getDisplayedItem().getItem() instanceof ItemMap)
-                    {
+                    if (itemFrame.getDisplayedItem().getItem() instanceof ItemMap) {
                         ItemMap map = (ItemMap) itemFrame.getDisplayedItem().getItem();
                         MapStorage mapStorage = Minecraft.getMinecraft().theWorld.getMapStorage();
                         Map<String, WorldSavedData> worldSavedDataMap = ReflectionHelper.getPrivateValue(MapStorage.class, mapStorage, "loadedDataMap", "field_75749_b");
-                        for (String st : worldSavedDataMap.keySet())
-                        {
-                            if (st.startsWith("map_"))
-                            {
+                        for (String st : worldSavedDataMap.keySet()) {
+                            if (st.startsWith("map_")) {
                                 Minecraft.getMinecraft().entityRenderer.getMapItemRenderer();
                                 MapData md = (MapData) worldSavedDataMap.get(st);
                                 BufferedImage icon = new BufferedImage(128, 128, BufferedImage.TYPE_INT_RGB);
@@ -76,20 +80,16 @@ public class CopyNBTHandler {
                                 DynamicTexture mapTexture = new DynamicTexture(128, 128);
                                 int[] mapTextureData = mapTexture.getTextureData();
 
-                                for (int i = 0; i < mapTextureData.length; ++i)
-                                {
+                                for (int i = 0; i < mapTextureData.length; ++i) {
                                     mapTextureData[i] = 0;
                                 }
-                                for (int i = 0; i < 16384; ++i)
-                                {
+                                for (int i = 0; i < 16384; ++i) {
                                     int j = mapData.colors[i] & 255;
 
-                                    if (j / 4 == 0)
-                                    {
+                                    if (j / 4 == 0) {
                                         mapTextureData[i] = (i + i / 128 & 1) * 8 + 16 << 24;
                                     }
-                                    else
-                                    {
+                                    else {
                                         mapTextureData[i] = MapColor.mapColorArray[j / 4].func_151643_b(j & 3);
                                     }
                                 }
@@ -105,12 +105,11 @@ public class CopyNBTHandler {
                 ClipboardHelper.setClipboard(entity.serializeNBT());
                 Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessage(new ChatComponentText(I18n.format("wikitools.message.copiedNBT")));
             }
-        } else
-        {
+        }
+        else {
             BlockPos pos = objectMouseOver.getBlockPos();
             TileEntity tile = minecraft.theWorld.getTileEntity(pos);
-            if (tile != null)
-            {
+            if (tile != null) {
                 ClipboardHelper.setClipboard(tile.serializeNBT());
                 Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessage(new ChatComponentText(I18n.format("wikitools.message.copiedNBT")));
             }
@@ -118,12 +117,10 @@ public class CopyNBTHandler {
     }
 
     @SubscribeEvent()
-    public void onKeyboardInputEvent(GuiScreenEvent.KeyboardInputEvent event)
-    {
+    public void onKeyboardInputEvent(GuiScreenEvent.KeyboardInputEvent event) {
         // Keybinds don't register in GUI's
         if (!Keyboard.isKeyDown(WikiToolsKeybinds.COPY_NBT.getKeyCode())) return;
-        if (event.gui instanceof GuiContainer)
-        {
+        if (event.gui instanceof GuiContainer) {
             Slot slot = ((GuiContainer) event.gui).getSlotUnderMouse();
             if (slot == null) return;
             if (!slot.getHasStack()) return;
